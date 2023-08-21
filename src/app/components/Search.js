d5fc57c66d66aeebed13bfe5506ac2ee9058bcf5
Jorge Matos <jisapjisap@gmail.com>
2023-08-21 19:37:31 +0200

    hero-search

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -2,6 +2,7 @@
 
 import { useContext } from "react"
 import { SearchContext } from "../context/search"
+import LocationSelection from "./LocationSelection";
 
 const Search = () => {
   
@@ -11,8 +12,11 @@ const Search = () => {
     <div className={`${searchActive 
       ? 'bg-white rounded-none xl:h-[80px]'
       : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'
-      } hidden xl:block w-full relative shadow-lg`}>
-      Search
+      } hidden xl:block w-full relative shadow-lg`}
+    >
+      <div className="xl:h-full flex items-center px-6 xl:px-0">
+        <LocationSelection />
+      </div>
     </div>
   )
 }
