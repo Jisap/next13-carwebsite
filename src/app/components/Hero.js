2ea367e8e6e377a2aef29383211d56a06b739b51
Jorge Matos <jisapjisap@gmail.com>
2023-08-25 20:12:25 +0200

    variants

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -4,6 +4,9 @@ import { useContext } from "react";
 import Search from "./Search";
 import { SearchContext } from "../context/search";
 import Image from "next/image";
+import { motion, easeInOut } from 'framer-motion';
+import { fadeIn } from '../../../variants';
+
 
 const Hero = () => {
 
@@ -18,14 +21,32 @@ const Hero = () => {
           
           {/* text */}
           <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
-            <h1 className="h1">
+            <motion.h1
+              variants={fadeIn('down', 0.2)} 
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{once: false, amount: 0.6}} 
+              className="h1"
+            >
               Explore the Finest <span className="text-accent">Global</span> Offers{''}
-            </h1>
-            <p className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10">
+            </motion.h1>
+            <motion.p 
+              variants={fadeIn('down', 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.6 }} 
+              className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10"
+            >
               Find your ideal ride for any adventure with our diverse range of affordable and dependable car rentals
-            </p>
+            </motion.p>
             {/* btns */}
-            <div className="flex gap-x-3 justify-center xl:justify-start">
+            <motion.div 
+              variants={fadeIn('down', 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.8 }} 
+              className="flex gap-x-3 justify-center xl:justify-start"
+            >
               <button className="btn-cta">
                 <Image 
                   src={'/icons/buttons/app-store.svg'} 
@@ -42,12 +63,18 @@ const Hero = () => {
                   alt=''
                 />
               </button>
-            </div>
+            </motion.div>
           </div>
 
           {/* img car */}
-          <div className="relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px]
-          xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px]:right-[120px] xl:top-48">
+          <motion.div 
+            variants={fadeIn('up', 0.6)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }} 
+            className="relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px]
+          xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px]:right-[120px] xl:top-48"
+          >
             <Image 
               src={'/images/hero/car.svg'} 
               alt='image car'
@@ -55,7 +82,7 @@ const Hero = () => {
               style={{objectFit: 'contain'}}
               priority
             />
-          </div>
+          </motion.div>
 
         </div>
 
@@ -63,12 +90,25 @@ const Hero = () => {
       
       {
         searchActive ? ( // Si el scrollY > 800 <Search /> justo debajo del Header
-          <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
+          <motion.div 
+            variants={fadeIn('down', 0.2)}
+            initial={{y: '-100%'}}
+            animate={{y: 0}}
+            transition={{ease: easeInOut}} 
+            className="fixed top-[80px] z-10 w-full max-w-[1920px]"
+          >
             <Search />
-          </div>
+          </motion.div>
         ) : ( // Sino se coloca al final de esta section en medio de esta misma y la siguiente
-          <div className="-mt-12 w-full max-w-[1300px] mx-auto">
-            <Search />
+            <div className="-mt-12 w-full max-w-[1300px] mx-auto">
+            <motion.div 
+                variants={fadeIn('up', 0.8)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.2 }}    
+            >
+              <Search />
+            </motion.div>
           </div>
         )
       }
