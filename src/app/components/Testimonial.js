6fb9bbf68004bad4b46e77e6db8173f67d994a3c
Jorge Matos <jisapjisap@gmail.com>
2023-08-29 20:40:47 +0200

    testimonial

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -1,8 +1,13 @@
+import TestimonialSlider from "./TestimonialSlider"
 
 
 const Testimonial = () => {
   return (
-    <section className="section flex items-center bg-cyan-300" id="testimonial">Testimonial</section>
+    <section className="section flex items-center" id="testimonial">
+      <div className="container mx-auto">
+        <TestimonialSlider />
+      </div>
+    </section>
   )
 }
 
