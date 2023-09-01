b5c226ffb3ee7eb4c80d8cbf6201ad8aec5395e8
Jorge Matos <jisapjisap@gmail.com>
2023-09-01 16:57:40 +0200

    footer

--- src/app/components/Copyright.js
+++ src/app/components/Copyright.js
@@ -2,7 +2,9 @@
 
 const Copyright = () => {
   return (
-    <div>Copyright</div>
+    <div className="text-center py-10 border-t text-sm">
+      Copyright &copy; Carland 2023. All rights reserved
+    </div>
   )
 }
 
