8b8ed84dd54c841ce5ef474d15ca5dd7c498f015
Jorge Matos <jisapjisap@gmail.com>
2023-09-01 17:26:40 +0200

    backToTop

--- src/app/components/Cta.js
+++ src/app/components/Cta.js
@@ -7,7 +7,7 @@ import { fadeIn } from '/variants';
 const Cta = () => {
   return (
     <section 
-      className="pt-24 xl:pt-48 flex items-end pb-0 bg-[#b2b7c2]/10" 
+      className="pt-24 xl:pt-48 flex items-end pb-0 bg-[#b2b7c2]/10 overflow-hidden" 
       id="cta"
     >
       <div className="container mx-auto">
@@ -62,7 +62,7 @@ const Cta = () => {
           </div>
           {/* image */}
           <motion.div
-            variants={fadeIn('up', 0.8)}
+            variants={fadeIn('left', 0.8)}
             initial='hidden'
             whileInView={'show'}
             viewport={{ once:false, amount:0.2}} 
